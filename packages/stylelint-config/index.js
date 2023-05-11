8fc73c65c994cc22c691b9467ea8c2c764ac6da5
JuPeng <1662345805@qq.com>
2023-05-11 20:07:51 +0800

    feat：新增stylelint配置

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  root: true,
   extends: [
     'stylelint-config-standard-scss',
     'stylelint-config-recommended-vue',
@@ -8,7 +9,7 @@ module.exports = {
     {
       customSyntax: 'postcss-html',
       files: [
-        '**/*.{htm,html,vue}',
+        '**/*.{html,vue}',
       ],
     },
     {
@@ -23,6 +24,27 @@ module.exports = {
     'stylelint-scss',
   ],
   rules: {
+    'indentation': 2,
+    'max-empty-lines': 1,
+    'no-empty-source': null,
+    'no-eol-whitespace': true,
+    'no-extra-semicolons': true,
+    'no-empty-first-line': true,
+    'string-quotes': 'single',
+    'no-missing-end-of-source-newline': true,
+    'alpha-value-notation': 'number',
+    'color-hex-case': 'lower',
+    'color-hex-length': 'short',
+    'color-function-notation': 'legacy',
+    'block-opening-brace-space-before': 'always',
+    'declaration-colon-space-after': 'always-single-line',
+    'declaration-colon-space-before': 'never',
+    'value-list-comma-space-after': 'always-single-line',
+    'value-list-comma-space-before': 'never',
+    'function-max-empty-lines': 0,
+    'function-comma-newline-after': 'never-multi-line',
+    'function-parentheses-space-inside': 'never',
+    'function-whitespace-after': 'always',
     'selector-class-pattern': [
       '^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$',
       {
