2d185a772f5bdd989d04ce58ea74c523083930a0
JuPeng <1662345805@qq.com>
2023-09-15 21:04:10 +0800

    release eslint-config to 0.2.2

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -6,12 +6,9 @@ module.exports = {
     'src/assets/**/*',
   ],
   rules: {
-    'vue/component-tags-order': [
-      'error',
-      {
-        order: [['script', 'template'], 'style'],
-      },
-    ],
+    'vue/block-order': ['error', {
+      order: [['script', 'template'], 'style'],
+    }],
     'curly': 'off',
     'antfu/if-newline': 'off',
     'vue/singleline-html-element-content-newline': 'off',
