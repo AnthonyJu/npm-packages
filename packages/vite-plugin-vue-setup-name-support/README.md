1402ca3aa87dcb6f5931542205b9762d55d73a6a
AnthonyJu <1662345805@qq.com>
2024-02-18 21:09:04 +0800

    feat: add support for setup name in vite-plugin-vue-setup-name-support

---
+++ packages/vite-plugin-vue-setup-name-support/README.md
@@ -0,0 +1,42 @@
+# vite-plugin-vue-setup-extend-plus
+
+Make the vue script setup syntax support the name and inheritAttrs attribute
+
+Automatically convert based on name and inheritAttrs
+
+## Feature
+
+- support name
+- support inheritAttrs
+
+## Install
+
+```bash
+pnpm add -D vite-plugin-vue-setup-name-support
+```
+
+## Usage
+
+- In `vite.config.ts`:
+
+```ts
+import { Plugin, defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import VueSetupNameSupport from 'vite-plugin-vue-setup-name-support'
+
+export default defineConfig({
+  plugins: [vue(), VueSetupNameSupport()],
+})
+```
+
+- In Vue file:
+
+```vue
+<template>
+  <div>vite-plugin-vue-setup-name</div>
+</template>
+
+<script lang="ts" setup name="MyName" inheritAttrs="false">
+
+</script>
+```
---
+++ packages/vite-plugin-vue-setup-name-support/src/index.ts
@@ -0,0 +1,47 @@
+import type { Plugin } from 'vite'
+import { compileScript, parse } from '@vue/compiler-sfc'
+import MagicString from 'magic-string'
+
+function supportSetupName(code: string, id: string) {
+  const magicString = new MagicString(code)
+  const { descriptor } = parse(code)
+  if (descriptor.scriptSetup && !descriptor.script) {
+    const { attrs } = compileScript(descriptor, { id })
+    const { name, lang, inheritAttrs } = attrs
+    if (name || inheritAttrs) {
+      magicString.appendLeft(
+        0,
+        `<script ${lang ? `lang="${lang}"` : ''}>
+          import { defineComponent } from 'vue'
+          export default defineComponent({
+            ${name ? `name: "${name}",` : ''}
+            ${inheritAttrs ? `inheritAttrs: ${inheritAttrs !== 'false'},` : ''}
+          })
+        </script>\n`,
+      )
+    }
+
+    const map = magicString.generateMap({ hires: true })
+    const filename = /.*\/(\S*)/.exec(id)![1]
+
+    map.file = filename
+    map.sources = [filename]
+
+    return {
+      map,
+      code: magicString.toString(),
+    }
+  }
+  return null
+}
+
+export default function vueSetupNameSupport(): Plugin {
+  return {
+    name: 'vite:vue-setup-name-support',
+    enforce: 'pre',
+    async transform(code, id) {
+      if (!/\.vue$/.test(id)) return null
+      return supportSetupName.call(this, code, id)
+    },
+  }
+}
